f89638655f8b17db839e98e730468477dfc976ad
denilsonbastidas <denilsonbastidas3@gmail.com>
2021-12-14 18:15:49 -0400

    actualizacion de version

--- src/app/components/proyectos/proyectos.component.ts
+++ src/app/components/proyectos/proyectos.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {ProjectExterna} from '../../services/cargaExterna';
+import {ProjectExterna} from '../../services/projects.service';
 
 @Component({
   selector: 'app-proyectos',
